3823802c983c679a55bcb26615e859f03b6b5682
Sam Sesh <admin@samsesh.net>
2023-04-21 14:18:36 +0330

    update ubuntu installer

--- install-on-ubuntu.sh
+++ install-on-ubuntu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo update repo please wait a few minutes 
+echo "update repo please wait a few minutes "
 apt-get update 
 clear
-echo install wireguard tools please wait a few minutes 
+echo "install wireguard tools please wait a few minutes "
 apt-get install -y wireguard-dkms wireguard-tools 
 modprobe wireguard
 clear
-echo install wg docker
+echo "install wg docker"
 source $PWD/.env
 mkdir -p $FILELOCATION
 cp -r ./src/* $FILELOCATION
 cp -r ./.env $FILELOCATION
 bash $FILELOCATION/run.sh
-echo your docker wg install at $FILELOCATION
+echo "your docker wg install at $FILELOCATION"
